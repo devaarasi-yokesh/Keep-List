3e9ee93807acf399e3724e5d849a0ec33acf43a5
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2024-10-30 23:49:34 +0530

    Update script.js
    - Managed functionality to create element dynamically.
    - Update the list value as per upcoming list.
    - Understood the concept of creating dynamic elements.

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const check = document.getElementById("item1-check");
-const val = document.getElementById("item-val")
-val.addEventListener("keyup",()=>{
-    console.log(val);
-    let a = document.createElement("ul");
-    a.style.backgroundColor = 'blue';
-    let b = document.createElement("li");
-    b.innerText = val.innerText;
-    a.appendChild(b);
+const check = document.querySelector(".container");
+const showContainer = document.querySelector(".show")
 
+const add = document.getElementById("addButton");
+
+add.addEventListener("click", () => {
+    const checkBox = document.createElement("input");
+    const inputVal = document.createElement("input");
+    checkBox.setAttribute("type","checkbox");
+    inputVal.setAttribute("type","text");
+    inputVal.setAttribute("id","val");
+    var k = inputVal.id;
+    console.log(inputVal.id)
+   check.appendChild(checkBox);
+   check.appendChild(inputVal);
+inputVal.addEventListener("keypress",(event)=>{
+    if(event.key == "Enter"){
+        let a = document.createElement("input");
+        a.setAttribute("type","checkbox");
+        let b = document.createElement("li");
+        b.style.listStyle = "none";
+        b.appendChild(a);
+        console.log(inputVal.value,a)
+        b.innerText = inputVal.value;
+        // const list = a.appendChild(b);
+        showContainer.append(a)
+        showContainer.appendChild(b);
+        inputVal.value = "";
+        a.addEventListener("click", ()=>{
+            b.style.textDecoration = "line-through";
+        })
+    }
+});
 })
+
